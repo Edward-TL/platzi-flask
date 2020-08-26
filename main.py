c846de60d08a61e4ac3d89c968e79dad0bcc2576
Edward-TL <edward.toledo.lopez@hotmail.com>
2020-08-26 17:46:06 -0500

    Project Started

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, make_response, redirect, render_template
+
+app = Flask(__name__)
+
+todos = ['Comprar cafe', 'Solicitud de compra', 'Entregar video al productor']
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html', error=error)
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+
+    # response_string = f'''Flask says: Kicite Joloche!\n
+    # tu IP es esta {user_ip}. ZAZ!''' 
+    
+    context = {
+        'user_ip' : user_ip,
+        'todos' : todos,
+    }
+
+    return render_template('hello.html', **context)
