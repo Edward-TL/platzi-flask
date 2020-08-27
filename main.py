e8fd89bb5428b8d340a6b4ee4ce58b27dcfead46
Edward-TL <edward.toledo.lopez@hotmail.com>
2020-08-27 15:01:34 -0500

    Creada la barra de notificaciones

--- main.py
+++ main.py
@@ -1,50 +1,63 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask import Forms, StringField, SubmitField, PasswordField
-from wtforms.fields import DataRequired
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-Bootstrap = Bootstrap(app)
+bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
-todos = ['Comprar cafe', 'Solicitud de compra', 'Entregar video al productor']
+
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
+
 
 class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
 
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
 
+
 @app.errorhandler(500)
-def internal_server_error(error):
+def server_error(error):
     return render_template('500.html', error=error)
+
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    # response.set_cookie('user_ip', user_ip)
     session['user_ip'] = user_ip
 
     return response
 
-@app.route('/hello')
-def hello():
-    # user_ip = request.cookies.get('user_ip')
-    user_ip = session.get('user_ip')
 
-    # response_string = f'''Flask says: Kicite Joloche!\n
-    # tu IP es esta {user_ip}. ZAZ!''' 
-    
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
     context = {
-        'user_ip' : user_ip,
-        'todos' : todos,
-        'login_form' : login_form
+        'user_ip': user_ip,
+        'username': username,
+        'todos': todos,
+        'login_form': login_form
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('¡Nombre de usuario registrado con éxito!')
+
+        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
