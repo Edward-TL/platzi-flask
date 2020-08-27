a068556fa270ebd10f283e842e9a72aef662cf1d
Edward-TL <edward.toledo.lopez@hotmail.com>
2020-08-27 01:30:32 -0500

    Avances del curso. Pendiente para Flask-WTF

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
+from flask_bootstrap import Bootstrap
+from flask import Forms, StringField, SubmitField, PasswordField
+from wtforms.fields import DataRequired
 
 app = Flask(__name__)
+Bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Comprar cafe', 'Solicitud de compra', 'Entregar video al productor']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -17,13 +28,15 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    # response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    # user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
 
     # response_string = f'''Flask says: Kicite Joloche!\n
     # tu IP es esta {user_ip}. ZAZ!''' 
@@ -31,6 +44,7 @@ def hello():
     context = {
         'user_ip' : user_ip,
         'todos' : todos,
+        'login_form' : login_form
     }
 
     return render_template('hello.html', **context)
